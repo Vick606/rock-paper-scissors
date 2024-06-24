338b7f64adfdcb4d7b7c80de54a178500b83fad6
Victor Koech <vickoch20@gmail.com>
2024-06-24 13:29:46 +0300

    Added the function to determine whether rock, paper, or scissors based on the randomly generated no

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getComputerChoice() {
     // Generate a random no btwn 0 and 2
     const randomNumber = Math.floor(Math.random() * 3);
+    // Return "rock", "paper", or "scissors" depending on the value randomly generated
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
